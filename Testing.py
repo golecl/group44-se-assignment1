42f7205efa3494f012b7c7cf4e0f26ed4e79b594
goleccc <113783833+golecl@users.noreply.github.com>
2022-10-03 17:26:09 +0100

    added tests for calculate()
    not merging to main until all tests pass

--- Testing.py
+++ Testing.py
@@ -1,5 +1,12 @@
 
 from Calculator import testFunction
 
-def test_test():
-    assert testFunction()==0
+def test_calculate():
+    assert calculate("2+2") == 4
+    assert calculate("4-2") == 2
+    assert calculate("2*3") == 6
+    assert calculate("2+3-4") == 1
+    assert calculate("2+2-3*3") == -5
+    assert calculate("15-12*2") == 6
+    assert calculate("23*3-5") == 64
+    assert calculate("200-100*0") == 200
