06611febcff9ee0b67ca7a82d8880d9a2b3a826d
goleccc <113783833+golecl@users.noreply.github.com>
2022-10-03 16:28:51 +0100

    added tests for subtraction
    not merging to main until all tests pass

--- Testing.py
+++ Testing.py
@@ -1,5 +1,8 @@
-
 from Calculator import testFunction
 
-def test_test():
-    assert testFunction()==0
+def test_subtraction():
+    assert calculate("23-23") == 0
+    assert calculate("23-23-1") == -1
+    assert calculate("-23-3") == -26
+    assert calculate("23-23*2") == -23
+    assert calculate("23+2-5") == 20
