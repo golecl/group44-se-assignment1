4a9fe6b1b14dbca6e3b6536c76b7fc8913824217
goleccc <113783833+golecl@users.noreply.github.com>
2022-10-03 13:21:17 +0100

    created the test for testing the input string
    will not merge to main so that yingzheng can merge and check if his tests pass

--- Testing.py
+++ Testing.py
@@ -1,5 +1,30 @@
+from Calculator import *
 
-from Calculator import testFunction
-
-def test_test()
+def test_test():
     assert testFunction()==0
+
+def test_validity():
+    assert checkIfValid("345+23-123*63463-23123") == True
+    assert checkIfValid("-123-1") == True
+    assert checkIfValid("973498-234+234*234") == True
+    assert checkIfValid("9738-234") == True
+    assert checkIfValid("324+234") == True
+    assert checkIfValid("2344*3432") == True
+    assert checkIfValid("-1231+3423") == True
+    assert checkIfValid("-1231*3423") == True
+
+    assert checkIfValid("123-") == False
+    assert checkIfValid("123/234") == False
+    assert checkIfValid("*23") == False
+    assert checkIfValid("-+234234") == False
+    assert checkIfValid("abc-2343") == False
+    assert checkIfValid("") == False
+    assert checkIfValid("2334**234") == False
+    assert checkIfValid("/4523") == False
+    assert checkIfValid("2334**234") == False
+    assert checkIfValid("+--34") == False
+    assert checkIfValid("+") == False
+    assert checkIfValid("abc") == False
+    assert checkIfValid("234.222+2342") == False
+    assert checkIfValid("238974_234+234)))234(2342)") == False
+    assert checkIfValid("(342*35345)-34234") == False
