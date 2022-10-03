e0b2dafbd3d342fa47c3cd3964d7606000908db4
goleccc <113783833+golecl@users.noreply.github.com>
2022-10-03 16:40:51 +0100

    testing multiplication
    added tests to see if multiplication is handled correctly. not committing to main until all tests pass

--- Testing.py
+++ Testing.py
@@ -1,5 +1,8 @@
-
 from Calculator import testFunction
 
-def test_test():
-    assert testFunction()==0
+def test_multiplication():
+    assert caclulate("234*2") == 468
+    assert caclulate("2*34*2") == 136
+    assert caclulate("2+234*2") == 470
+    assert caclulate("2-234*2") == -466
+    assert caclulate("20*0") == 0
