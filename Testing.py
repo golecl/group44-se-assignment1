c10b6f282e538c03e596d88dae32c019ba1c9b0d
goleccc <113783833+golecl@users.noreply.github.com>
2022-10-03 17:53:05 +0100

    testing the calculator fucntion
    not merging to main until all tests pass

--- Testing.py
+++ Testing.py
@@ -1,5 +1,8 @@
-
 from Calculator import testFunction
 
-def test_test():
-    assert testFunction()==0
+def test_Calculator():
+    assert Calculator("abcd") == "Sorry, this input is invalid"
+    assert Calculator("9+10") == "The answer is 19"
+    assert Calculator("10-9+2*23") == "The answer is 47"
+    assert Calculator("10/2") == "Sorry, this input is invalid"
+    assert Calculator("") == "Sorry, this input is invalid"
