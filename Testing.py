c3de4f60bdacc78617199de61f817d9dc330e91a
golecl <113783833+golecl@users.noreply.github.com>
2022-10-07 13:40:45 +0100

    added the calculator code (#11)
    * created the test for testing the input string
    
    will not merge to main so that yingzheng can merge and check if his tests pass
    
    * removed unnecessary test
    
    * added tests for addition
    
    testing addition , not merging to main yet
    
    * updated test
    
    * added all tests to branch
    
    * Update Calculator.py
    
    Nearly finish, but not fully.
    
    * fixed the calculator so that it passes all tests
    
    * Update Calculator.py
    
    add some details in the file, commit the final version of the Calculator file.
    
    * final calculator code, passes all tests
    
    * Revert "Merge branch 'testingSubtraction' into calculatorCode"
    
    This reverts commit 93e5ad0b2d355be76c5c7080680838f0a9865f3f, reversing
    changes made to de69dc1a152d64377caeb8fec794e637b837f7de.
    
    * Revert "Merge branch 'testingStringValidity' into calculatorCode"
    
    This reverts commit de69dc1a152d64377caeb8fec794e637b837f7de, reversing
    changes made to 326ab25e0faef306ea74094a8d0752c14a19aa93.
    
    * Revert "Merge branch 'testingAddition' into calculatorCode"
    
    This reverts commit 326ab25e0faef306ea74094a8d0752c14a19aa93, reversing
    changes made to 65f1fc57f230bb8ac40b76b86864355e370b8902.
    
    * fixed typo
    
    * deleted all unnecessary files
    
    Co-authored-by: ying9999 <99326187+ying9999@users.noreply.github.com>

--- Testing.py
+++
@@ -1,8 +0,0 @@
-from Calculator import *
-
-def test_subtraction():
-    assert calculate("23-23") == 0
-    assert calculate("23-23-1") == -1
-    assert calculate("-23-3") == -26
-    assert calculate("23-23*2") == -23
-    assert calculate("23+2-5") == 20
