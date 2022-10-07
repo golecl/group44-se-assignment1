ebd0db12f78374003fd2df46863564239dd4ecc8
goleccc <113783833+golecl@users.noreply.github.com>
2022-10-07 13:33:50 +0100

    Revert "Merge branch 'testingStringValidity' into calculatorCode"
    This reverts commit de69dc1a152d64377caeb8fec794e637b837f7de, reversing
    changes made to 326ab25e0faef306ea74094a8d0752c14a19aa93.

--- Testing.py
+++ Testing.py
@@ -1,27 +1,7 @@
 from Calculator import *
 
-def test_validity():
-    assert checkIfValid("345+23-123*63463-23123") == True
-    assert checkIfValid("-123-1") == True
-    assert checkIfValid("973498-234+234*234") == True
-    assert checkIfValid("9738-234") == True
-    assert checkIfValid("324+234") == True
-    assert checkIfValid("2344*3432") == True
-    assert checkIfValid("-1231+3423") == True
-    assert checkIfValid("-1231*3423") == True
-
-    assert checkIfValid("123-") == False
-    assert checkIfValid("123/234") == False
-    assert checkIfValid("*23") == False
-    assert checkIfValid("-+234234") == False
-    assert checkIfValid("abc-2343") == False
-    assert checkIfValid("") == False
-    assert checkIfValid("2334**234") == False
-    assert checkIfValid("/4523") == False
-    assert checkIfValid("2334**234") == False
-    assert checkIfValid("+--34") == False
-    assert checkIfValid("+") == False
-    assert checkIfValid("abc") == False
-    assert checkIfValid("234.222+2342") == False
-    assert checkIfValid("238974_234+234)))234(2342)") == False
-    assert checkIfValid("(342*35345)-34234") == False
+def test_addition():
+    assert calculate("2+2") == 4
+    assert calculate("324234+234+123") == 324591
+    assert calculate("23+12*2") == 47
+    assert calculate("23-12+3") == 14
